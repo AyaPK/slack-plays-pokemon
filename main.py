9bd8a376de40c8968b246f6c2a15d46ec0b309b0
AyaPK <Sherbnya@gmail.com>
2024-07-06 19:41:37 +0100

    Have bot make basic reply when mentioned

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from dotenv import load_dotenv
 
+load_dotenv()
+app = App(token=os.getenv('SLACK_TOKEN'))
+
 
 def main():
-    load_dotenv()
+    start_slack_bot()
 
-    print(os.getenv("SLACK_TOKEN"))
+
+def start_slack_bot():
+    SocketModeHandler(app,
+                      os.getenv("SLACK_XAPP")
+                      ).start()
+
+
+@app.event("app_mention")
+def hello_world(ack, body, say, client):
+    say("I heard that!")
 
 
 if __name__ == "__main__":
