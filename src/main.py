072a960891ab211fa259d7bdca86156eb055d855
AyaPK <Sherbnya@gmail.com>
2024-07-09 21:44:56 +0100

    Add inputs to a CSV file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
+import csv
+import os
+
 from bot.slack_bot import start_slack_bot
 
 
 def main():
+    set_up_files()
     start_slack_bot()
+
+
+def set_up_files():
+    if not os.path.exists("data/inputs.csv"):
+        with open("data/inputs.csv", 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["order_id", "Input"])
+        print("Inputs CSV file created.")
 
 
 if __name__ == "__main__":
